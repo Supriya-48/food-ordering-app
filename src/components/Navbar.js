310c9b4947f844a7c43bb22f85c2a32138e5e7ce
Supriya Muppu <supriya.mupputhoughtworks.com@Supriyas-MacBook-Pro.local>
2022-12-28 20:06:00 +0530

    deployment

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,14 +17,14 @@ function Navbar(){
             <div className="leftSide" id = {openLinks ? "open" : "close"}>
                 <img src={logo} />
                 <div className="hiddenLinks">
-                <Link to= "/">Home</Link>
+                <Link to="/food-ordering-app/">Home</Link>
                 <Link to= "/menu">Menu</Link>
                 <Link to= "/about">About Us</Link>
                 <Link to= "/contact">Contact</Link>
                 </div>
             </div>
             <div className="rightSide">
-                <Link to= "/">Home</Link>
+                <Link to= "/food-ordering-app/">Home</Link>
                 <Link to= "/menu">Menu</Link>
                 <Link to= "/about">About Us</Link>
                 <Link to= "/contact">Contact</Link>
