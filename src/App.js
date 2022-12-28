2bd17fd6f15a0b1bece6b166ac2e514f6fe0048f
Supriya Muppu <supriya.mupputhoughtworks.com@Supriyas-MacBook-Pro.local>
2022-12-28 17:42:06 +0530

    Created navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
-
+import Navbar from './components/Navbar';
+import {BrowserRouter as Router,Switch,Route} from 'react-router-dom'
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
-      <h2>Hello</h2>
+      <Router>
+        <Navbar/>
+      </Router>
+      
     </div>
   );
 }
