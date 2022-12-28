b2bee3e174d714a2e120c91f810075cf8764511f
Supriya Muppu <supriya.mupputhoughtworks.com@Supriyas-MacBook-Pro.local>
2022-12-28 19:30:09 +0530

    Created contact page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './pages/Home';
 import Menu from './pages/Menu';
 import About from './pages/About'
 import Footer from './components/Footer';
+import Contact from './pages/Contact';
 function App() {
   return (
     <div className="App">
@@ -14,6 +15,7 @@ function App() {
           <Route path="/" element={<Home/>}/>
           <Route path="/menu" element={<Menu/>}/>
           <Route path="/about" element={<About/>}/>
+          <Route path="/contact" element={<Contact/>}/>
         </Routes>
         <Footer/>
       </Router>
